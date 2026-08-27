d4722fd5c641697cdd8c7341ec33f264570e7e24
Jeyabharathi A <ajeyabharathi12@gmail.com>
2026-08-27 19:45:37 +0530

    Fix CORS configuration

--- backend/src/main/java/smartbus_backend/config/SecurityConfig.java
+++ backend/src/main/java/smartbus_backend/config/SecurityConfig.java
@@ -71,7 +71,7 @@ public class SecurityConfig {
 
         configuration.setAllowedOrigins(List.of(
                 "http://localhost:8000",
-                "http://127.0.0.1:8000"
+                "http://127.0.0.1:8000",
                  "https://a-jeyabharathi.github.io"
         ));
 
